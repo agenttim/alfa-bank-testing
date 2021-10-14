05b4eb406d81f12ddcbfd8de746800254d96eb42
agent_tim <agent_tim@mail.ru>
2021-10-14 23:44:00 +0500

    Initial commit

---
+++ src/test/java/com/example/alfabanktesting/AlfaBankTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfabanktesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaBankTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
